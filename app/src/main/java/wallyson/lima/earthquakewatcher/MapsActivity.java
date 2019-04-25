e4e471e5ea1c8d850f76a19bc2a700a95d5cd7d3
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-04-25 09:48:38 -0300

    Change some coe MapsActivity

--- app/src/main/java/wallyson/lima/earthquakewatcher/MapsActivity.java
+++ app/src/main/java/wallyson/lima/earthquakewatcher/MapsActivity.java
@@ -284,6 +284,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         StringBuilder stringBuilder = new StringBuilder();
 
                         try {
+
+                            if ( response.has("tectonicSummary") && response.getString("tectonicSummary") != null ) {
+                                JSONObject tectonic = response.getJSONObject("tectonicSummary");
+
+                                if ( tectonic.has("text") && tectonic.getString("text") != null ) {
+                                    String text = tectonic.getString("text");
+
+                                    htmlPop.loadDataWithBaseURL(null, text, "text/html", "UTF-8", null);
+                                }
+                            }
+
                             JSONArray cities = response.getJSONArray("cities");
 
                             for( int i = 0; i < cities.length(); i++ ) {
